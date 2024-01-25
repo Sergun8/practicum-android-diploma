de15f7705f075390cba3f57b91c6d78ebd070cf6
zhek1chan <evg.ermolov@yandex.ru>
2024-01-25 12:21:49 +0500

    +classes

---
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/ConvertorOfClasses.kt
@@ -0,0 +1,5 @@
+package ru.practicum.android.diploma.data.dto
+
+class ConvertorOfClasses {
+    // TODO: ("заглушка, вдруг не понадобится конвертер, запрос возможно сам всё будет записывать в классы в слое models, без проблем")
+}
--- app/src/main/java/ru/practicum/android/diploma/domain/models/object/Experience.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/object/Experience.kt
@@ -1,4 +1,6 @@
 package ru.practicum.android.diploma.domain.models.`object`
 
-class Experience {
-}
+data class Experience(
+    val id: String?,
+    val name: String?
+)
