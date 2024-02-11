19ee458583965b8589044c14a123bdd1816fbd53
zhek1chan <evg.ermolov@yandex.ru>
2024-02-12 02:04:29 +0500

    Коин повержен

--- app/src/main/java/ru/practicum/android/diploma/data/favourites/GetDataRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/favourites/GetDataRepositoryImpl.kt
@@ -2,17 +2,17 @@ package ru.practicum.android.diploma.data.favourites
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import ru.practicum.android.diploma.data.room.VacancyDao
+import ru.practicum.android.diploma.data.room.AppDatabase
 import ru.practicum.android.diploma.data.room.VacancyShortMapper
 import ru.practicum.android.diploma.domain.api.GetDataRepository
 import ru.practicum.android.diploma.domain.models.Vacancy
 
 class GetDataRepositoryImpl(
-    private val vacancyDao: VacancyDao
+    private val db: AppDatabase
 ) : GetDataRepository {
 
     override fun get(): Flow<List<Vacancy>?> =
-        vacancyDao.getVacancyList().map { list ->
+        db.vacancyDao().getVacancyList().map { list ->
             list.map { item ->
                 VacancyShortMapper.map(item)
             }
