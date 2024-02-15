c1c2e32027cd83a387cdda542a86add88f9e22ef
Sergun <ya.tarannov@yandex.ru>
2024-02-16 00:01:19 +0300

    Epic0

--- app/src/main/java/ru/practicum/android/diploma/ui/team/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/team/TeamFragment.kt
@@ -1,11 +1,13 @@
 package ru.practicum.android.diploma.ui.team
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
 
 class TeamFragment : Fragment() {
-/*
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,15 +21,4 @@ class TeamFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_team, container, false)
     }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TeamFragment().apply {
-                arguments = Bundle().apply {
-                }
-            }
-    }
-
- */
 }
